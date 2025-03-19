64f273abcd3e875e7e47a4b2e2bc6dcaf9d098ee
aiden_hichens <aiden_hichens@paraparaumucollege.school.nz>
2025-03-20 11:43:15 +1300

    added the first iteration of adding tuples on the screen and in a list

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ class Questionnaire:
     def start(self):
         start = True
         while start == True:
+            question = "Do you wish to make a tuple point"
+            answer = (input(question)).lower()
+            if answer == "yes":
+                self.turtle.save_tuple()
+            elif answer == "quit" or "break":
+                start = False
+                print(self.turtle)
+            else:
+                pass
             question = "Which direction do you want to move in?"
             answer = (input(question)).lower()
             if answer == "up":
@@ -19,8 +28,11 @@ class Questionnaire:
                 self.turtle.move_right()
             elif answer == "left":
                 self.turtle.move_left()
-            else:
+            elif answer == "quit" or "break":
                 start = False
+                print(self.turtle)
+            else:
+                pass
 
 
 class PointerTurtle:
@@ -28,6 +40,10 @@ class PointerTurtle:
         self.pointer_turtle = Turtle()
         self.current_y = 0
         self.current_x = 0
+        self.tuple_list = []
+
+    def __str__(self):
+        return str(self.tuple_list)
 
     def move_up(self):
         self.current_y = self.current_y + 20
@@ -44,6 +60,10 @@ class PointerTurtle:
     def move_left(self):
         self.current_x = self.current_x - 20
         self.pointer_turtle.goto(self.current_x, self.current_y)
+
+    def save_tuple(self):
+        current_tuple = (self.current_x, self.current_y)
+        self.tuple_list.append(current_tuple)
 
 
 if __name__ == '__main__':
